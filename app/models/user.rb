a2b6681aa596b29d5e3550ca007fa0f21a4aa3da
Deer-Garden <willkinsongs.alais@gmail.com>
2021-01-10 16:48:48 +0900

    ルーティングエラーの解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :main_menus
+  
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password , with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+    validates :password_confirmation
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root to: "main_menus#index"
   resources :main_menus
 
-  root to: "main_menus#index"
+ 
 
 end
