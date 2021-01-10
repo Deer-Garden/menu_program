0980eff2675ef67ce60a8a17d486c5ff7bd8c3e9
Deer-Garden <willkinsongs.alais@gmail.com>
2021-01-10 16:01:44 +0900

    Active Storageの導入

--- app/models/main_menu.rb
+++ app/models/main_menu.rb
@@ -1,2 +1,13 @@
 class MainMenu < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name 
+    validates :expiration
+    validates :ingredient
+    validates :recipe
+    validates :kcal
+    validates :image
+  end
 end
--- db/migrate/20210110035521_create_main_menus.rb
+++ db/migrate/20210110035521_create_main_menus.rb
@@ -1,7 +1,12 @@
 class CreateMainMenus < ActiveRecord::Migration[6.0]
   def change
     create_table :main_menus do |t|
-
+      t.string :name        ,null: false      ,default: ""
+      t.string :expiration  ,null: false      ,default: ""
+      t.text :ingredient    ,null: false 
+      t.text :recipe        ,null: false 
+      t.integer :kcal       ,null: false 
+      t.references :user    ,null: false      ,foreign_key: true
       t.timestamps
     end
   end
